5b1420cae5fed0b61d9255640771ee75a8ea9b5a
unknown <rajat.bansal631@gmail.com>
2019-12-26 21:19:12 +0530

    first commit

---
+++ file_generator.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	FILE *fil;
+	file=fopen("data.txt","w");
+	for(int i=0; i<50;i++){
+
+		fprintf(fil,"%d,%d,%d",i+1,rand()%100,rand()%100);
+	}
+	fclose(fil);
+	return 0;
+
+}
---
+++ make.mk
@@ -0,0 +1,4 @@
+rm -rf file_generator
+g++ -g file_generator.cpp -o file_generator
+./file_generator
+python read_data.py
